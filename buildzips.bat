a635f0632cc6fa9a0d037dbc265f1e5e3de75e00
Brad Robinson <brobinson@toptensoftware.com>
2008-10-03 10:38:48 +1000

    zip building

---
+++ buildzips.bat
@@ -0,0 +1,8 @@
+del SimpleLib.zip
+wzzip SimpleLib.zip ReadMe.txt SimpleLib.cpp SimpleLib.h TestCases\SimpleTest.cpp
+del SimpleLibPlus.zip
+wzzip SimpleLibPlus.zip ReadMe.txt SimpleLib.cpp SimpleLib.h TestCases\SimpleTest.cpp *.sln -r -P *.vcproj *.cpp *.h *.bmp *.rc -xProfileFileTest\*.*
+
+
+
+
